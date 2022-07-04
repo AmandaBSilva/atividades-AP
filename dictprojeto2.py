7a199d080826f2640d9df9d5471b37032979863c
AmandaBSilva <81535560+AmandaBSilva@users.noreply.github.com>
2022-07-04 15:16:30 -0300

    Update dictprojeto2.py

--- dictprojeto2.py
+++ dictprojeto2.py
@@ -1,1 +1,15 @@
+def celkel(temp):
+    return temp + 273.15
 
+def kelcel(temp):
+    return temp - 273.15
+
+def celfar(temp):
+    return temp * 9 / 5 + 32
+
+def farcel(temp):
+    return (temp - 32)* 5/9
+
+dicio = {
+    1: celker,
+}
