0f9965e50fccd0be569b34a0ceb6ed0d3df39e3f
AmandaBSilva <81535560+AmandaBSilva@users.noreply.github.com>
2022-07-04 13:01:31 -0300

    Update projeto2.py

--- projeto2.py
+++ projeto2.py
@@ -1,4 +1,6 @@
 opcao = input("Escolha uma das opções acima: ")
+if opcao == 0:
+    return
 temp = float(input("Digite o valor da temperatura em graus [Celsius | Kelvin | Fahrenheit]: "))
 if opcao == 1:
     print(str(temp+273.15)+"K°")
