24e702fab153e1f8a2bfac33f949cb5a815c49d8
AmandaBSilva <81535560+AmandaBSilva@users.noreply.github.com>
2022-07-04 15:19:45 -0300

    Update dictprojeto2.py

--- dictprojeto2.py
+++ dictprojeto2.py
@@ -11,5 +11,11 @@ def farcel(temp):
     return (temp - 32)* 5/9
 
 dicio = {
+    0: exit,
     1: celker,
+    2: celfar,
+    3: kelcel,
+    4: lambda t: celfar(kelcel(t)),
+    5: farcel,
+    6: lambda t; celkel(farcel(t))
 }
